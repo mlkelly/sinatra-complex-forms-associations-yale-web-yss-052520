1a6de8920a41df782f45f210a1925aaa03c2e2cb
mlkelly <maurak87@gmail.com>
2020-07-06 01:01:41 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 # Add seed data here. Seed your database with `rake db:seed`
+Owner.destroy_all
+Pet.destroy_all
+
 sophie = Owner.create(name: "Sophie")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
